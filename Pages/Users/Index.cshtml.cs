35abea1488db686bc6afbb00ec7a0b801f4b3207
Miguel Aguirre <miguelangelbernal09@gmail.com>
2025-05-27 17:05:06 -0500

    Users Index Creation
    Index Creation

---
+++ Pages/Users/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Proyect_EventPlanner.Data;
+using Proyect_EventPlanner.Models;
+
+namespace Proyect_EventPlanner.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EventContext _context;
+
+        public IndexModel(EventContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> Users { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Users != null)
+            {
+                Users = await _context.Users.ToListAsync();
+            }
+        }
+    }
+}
