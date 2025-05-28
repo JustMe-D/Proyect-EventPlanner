83fc9af4ef30386a0371bf07125fe8952427aad1
Santiago Cardenas <santiportal16@gmail.com>
2025-05-27 22:44:27 -0500

    Create

---
+++ Pages/Resources/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyect_EventPlanner.Pages.Resources
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
