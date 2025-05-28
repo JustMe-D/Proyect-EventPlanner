8651e16d33b5cba94b3aa2a6e44fdcd2eb39e5ed
Santiago Cardenas <santiportal16@gmail.com>
2025-05-27 22:06:49 -0500

    index Resource

---
+++ Pages/Resources/Index.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Proyect_EventPlanner.Data;
+using Proyect_EventPlanner.Models;
+
+namespace Proyect_EventPlanner.Pages.Resources
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EventContext _context;
+
+        public IndexModel(EventContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Resource> ResourceList { get; set; } =  default!;
+
+        public async Task OnGetAsync()
+        {
+            ResourceList = await _context.Resources.ToListAsync();
+        }
+    }
+}
