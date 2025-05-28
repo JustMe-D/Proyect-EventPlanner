bf4ea3fb34d2ebc40fee217bf5546aa34a67d770
Miguel Angel Delgado Serna <miguel3519.delgado@gmail.com>
2025-05-27 20:30:03 -0500

    index Locations

---
+++ Pages/Locations/Index.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Proyect_EventPlanner.Data;
+using Proyect_EventPlanner.Models;
+
+namespace Proyect_EventPlanner.Pages.Locations
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EventContext _context;
+
+        public IndexModel(EventContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Location> Locations { get; set; } = new List<Location>();
+
+        public async Task OnGetAsync()
+        {
+            Locations = await _context.Locations.ToListAsync();
+        }
+    }
+}
