d083f4ce8e62a0b42d31c7914cd8f2d9c11a95ac
Santiago Cardenas <santiportal16@gmail.com>
2025-05-28 10:04:54 -0500

    Delete Resources

---
+++ Pages/Resources/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Proyect_EventPlanner.Data;
+using Proyect_EventPlanner.Models;
+
+namespace Proyect_EventPlanner.Pages.Resources
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly EventContext _context;
+
+        public DeleteModel(EventContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Resource Resource { get; set; }
+        
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Resource = await _context.Resources
+                .Include(r => r.Event)
+                .FirstOrDefaultAsync(m => m.IdResource == id);
+
+            if (Resource == null)
+            {
+                return NotFound();
+            }
+
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+             var Resource = await _context.Resources.FindAsync(id);
+
+            if (Resource != null)
+            {
+                _context.Resources.Remove(Resource);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
